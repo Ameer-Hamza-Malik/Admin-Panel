fcd4b8914404e82197686e8b3c4f45f6a283afb3
Meer-Hamza <hm978835@gmail.com>
2024-06-15 15:57:54 +0500

    Initial commit

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>@Malik Enterprises Ltd 2024</p>
+    </footer>
+  );
+};
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,142 @@
+import { useState,useEffect } from "react";
+import { useAuth } from "../store/auth";
+
+const defaultContactFormData = {
+  username: "",
+  email: "",
+  message: "",
+};
+
+export const Contact = () => {
+  const [contact, setContact] = useState(defaultContactFormData);
+
+  const [userData, setUserData] = useState(true);
+
+  const { user, API } = useAuth();
+useEffect(()=>{
+  if (userData && user) {
+    setContact({
+      username: user.username,
+      email: user.email,
+      message: "",
+    });
+
+    setUserData(false);
+  }
+},[user,userData]);
+  // lets tackle our handleInput
+  const handleInput = (e) => {
+    const name = e.target.name.toLowerCase();
+    const value = e.target.value;
+
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
+
+  // handle fomr getFormSubmissionInfo
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:5001/api/form/contact`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(contact),
+      });
+
+      if (response.ok) {
+        setContact(defaultContactFormData);
+        const data = await response.json();
+        console.log(data);
+        alert("Message send successfully");
+      }
+    } catch (error) {
+      alert("Message not send");
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">contact us</h1>
+        </div>
+        {/* contact page main  */}
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img src="/images/support.png" alt="we are always ready to help" />
+          </div>
+
+          {/* contact form content actual  */}
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="Username"
+                  placeholder="Username"
+                  id="username"
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="Email"
+                  placeholder="Email"
+                  id="email"
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="message">message</label>
+                <textarea
+                  name="Message"
+                  placeholder="Type your message"
+                  id="message"
+                  autoComplete="off"
+                  value={contact.message}
+                  onChange={handleInput}
+                  required
+                  cols="30"
+                  rows="6"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit">submit</button>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <section className="mb-3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3592.9104511964597!2d68.67407217446251!3d25.77352120811833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x394befdd964b009d%3A0x8d28d3ce40390bc8!2sMalik%20Enterprises!5e0!3m2!1sen!2s!4v1708242158022!5m2!1sen!2s"
+            width="100%"
+            height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </section>
+      </section>
+    </>
+  );
+};
+
